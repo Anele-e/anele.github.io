e5dfbee0d7197e575cdb8ee91a9e04c8a389e9e3
Anele Dindili <aneledindil@gmail.com>
2026-02-10 23:11:52 +0200

    fix: minor navbar issue

--- personal-site/src/components/NavBar/NavBar.jsx
+++ personal-site/src/components/NavBar/NavBar.jsx
@@ -23,10 +23,10 @@ export default function NavBar() {
                 </div>
 
                 <ul className={styles.navLinks}>
-                    <li><a href="#about">About</a></li>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#education">Education</a></li>
                     <li><a href="#projects">Projects</a></li>
                     <li><a href="#contact">Contact</a></li>
-                    <li><a href="#education">Education</a></li>
                 </ul>
 
                 <button className={styles.burger}
